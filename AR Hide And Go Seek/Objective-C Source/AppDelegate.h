bb08c44be762986c2c7ecad56e5d8163c85781c3
Samuel Dong <Samuel.Dong@Outlook.com>
2015-11-18 20:59:49 -0800

    Added Missing Files

---
+++ AR Hide And Go Seek/Objective-C Source/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AR Hide And Go Seek
+//
+//  Created by Samuel Dong on 11/18/15.
+//  Copyright © 2015 SamuelNatalyRussell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AR Hide And Go Seek/Objective-C Source/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  AR Hide And Go Seek
+//
+//  Created by Samuel Dong on 11/18/15.
+//  Copyright © 2015 SamuelNatalyRussell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface GameViewController : GLKViewController
+
+@end
