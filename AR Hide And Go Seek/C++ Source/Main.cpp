f5d94a4c2423501b8fe4f7c879ad04da19eb1b15
Samuel Dong <Samuel.Dong@Outlook.com>
2015-11-30 20:26:20 -0800

    Added Main C++ Class

---
+++ AR Hide And Go Seek/C++ Source/Main.cpp
@@ -0,0 +1,9 @@
+//
+//  Main.cpp
+//  AR Hide And Go Seek
+//
+//  Created by Samuel Dong on 11/30/15.
+//  Copyright © 2015 SamuelNatalyRussell. All rights reserved.
+//
+
+#include "Main.hpp"
---
+++ AR Hide And Go Seek/C++ Source/Main.hpp
@@ -0,0 +1,14 @@
+//
+//  Main.hpp
+//  AR Hide And Go Seek
+//
+//  Created by Samuel Dong on 11/30/15.
+//  Copyright © 2015 SamuelNatalyRussell. All rights reserved.
+//
+
+#ifndef Main_hpp
+#define Main_hpp
+
+#include <stdio.h>
+
+#endif /* Main_hpp */
